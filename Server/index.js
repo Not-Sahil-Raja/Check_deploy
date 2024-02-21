ecc85b4e98ede8f7fd6ccd8c1d10fc78682ae41b
Not-Sahil-Raja <sahilraja2002@gmail.com>
2024-02-21 23:08:53 +0530

    all

---
+++ Server/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+let PORT = 5000;
+
+const app = express();
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
